4a619b11878bc2e3c95647d7c53ba4ab6331ab73
Estefania Cassingena Navone <85124911+estefaniacn@users.noreply.github.com>
2022-08-04 10:33:17 -0400

    Add link to course on YouTube

--- 2 - Modulos de Node/9 - El Modulo fs/app.js
+++ 2 - Modulos de Node/9 - El Modulo fs/app.js
@@ -24,7 +24,7 @@ fs.readFile('index.html', 'utf-8', (err, contenido) => {
 console.log('Despues de leer el archivo...');
 
 // Cambiar el nombre de un archivo.
-
+/*
 fs.rename('index.html', 'main.html', (err) => {
   if (err) {
     throw err;
@@ -37,7 +37,7 @@ console.log('Depues de cambiar el nombre del archivo...');
 // Agregar contenido al final de un archivo. 
 // Crear un archivo nuevo si no existe.
 
-fs.appendFile('index.html', '<p>Hola</p>', (err) => {
+fs.appendFile('main.html', '<p>Hola</p>', (err) => {
   if (err) {
     throw err;
   }
@@ -49,7 +49,7 @@ console.log('Depues de agregar contenido al archivo...');
 // Reemplazar contenido del archivo. 
 // Crear un archivo nuevo si no existe.
 
-fs.writeFile('index.html', 'Contenido nuevo', (err) => {
+fs.writeFile('main.html', 'Contenido nuevo', (err) => {
   if (err) {
     throw err;
   }
@@ -67,12 +67,12 @@ fs.unlink('main.html', (err) => {
   console.log('Archivo eliminado');
 });
 
-console.log('Depues de eliminar un archivo...');
+console.log('Despues de eliminar un archivo...');
 
 // -------------------------
 // Versiones sincronas.
 // -------------------------
-
+/*
 const archivo = fs.readFileSync('index.html', 'utf-8');
 
 console.log(archivo);
